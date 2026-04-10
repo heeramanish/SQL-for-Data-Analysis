4f2351a901666812180a42143dd75c1b26419b90
heeramanish <heera.manish143@gmail.com>
2026-04-10 18:46:21 +1000

    update

--- Project_sql/1_top_paying_jobs.sql
+++ Project_sql/1_top_paying_jobs.sql
@@ -7,10 +7,12 @@ Focuses on job postings with specified salaries (remove nulls)
 
 SELECT 
     job_title_short AS JOB_NAME,
+    CD.NAME AS EMPLOYER,
     salary_year_avg AS SALARY,
     job_schedule_type,
     job_posted_date::DATE AS JOB_POST_DATE -- FETCHING ONLY DATES WITHOUT TIMESTAMP
-FROM job_postings_fact 
+FROM job_postings_fact JPF   
+LEFT JOIN company_dim CD ON CD.company_id = JPF.company_id
 WHERE job_title_short = 'Data Analyst' AND job_work_from_home = 'TRUE' AND salary_year_avg IS NOT NULL
 ORDER BY salary_year_avg DESC
 LIMIT 10;
