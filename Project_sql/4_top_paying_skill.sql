ed53bb3d0b2e61b2a534557933cb5bb46613bbb9
heeramanish <heera.manish143@gmail.com>
2026-04-11 20:07:11 +1000

    UPDATE

---
+++ Project_sql/4_top_paying_skill.sql
@@ -0,0 +1,15 @@
+/*
+WHAT ARE TOP SKILLS BASED ON SALARY
+*/
+
+SELECT 
+    --COUNT(SJD.job_id) AS DEMAND_COUNT,
+    AVG(JPF.salary_year_avg) AS SALARY,
+    SD.skills AS SKILL
+    FROM job_postings_fact JPF
+LEFT JOIN skills_job_dim SJD ON SJD.job_id = JPF.job_id
+LEFT JOIN skills_dim SD ON SD.skill_id = SJD.skill_id
+WHERE job_title_short = 'Data Analyst' AND salary_year_avg IS NOT NULL
+GROUP BY SKILL
+ORDER BY  SALARY DESC
+LIMIT 10
