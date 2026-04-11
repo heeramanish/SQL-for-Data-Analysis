e626b674d9f08e329b92f6990c3e83c7b0c1a608
heeramanish <heera.manish143@gmail.com>
2026-04-11 19:51:13 +1000

    UPDATE

--- Project_sql/3_top_demanded_skills.sql
+++ Project_sql/3_top_demanded_skills.sql
@@ -1,18 +1,31 @@
 /*
- WHAT ARE THE TOP DEMANDED SKILLS
+ WHAT ARE THE TOP DEMANDED skills FOR DATA ANALYST
 */
 
 
 
-WITH SKILL_SETS AS
-(SELECT 
-    DISTINCT SD.skills AS SKILLS, SD.skill_id
-    --STRING_AGG(SD.skills,',') AS SKILLS
-    FROM skills_dim SD
-INNER JOIN skills_job_dim SJD ON SJD.skill_id = SD.skill_id
---GROUP BY SD.skill_id
-ORDER BY SD.skill_id ASC
-LIMIT 10)
+-- WITH SKILL_SETS AS
+-- (SELECT 
+--     DISTINCT SD.skills AS SKILLS, SD.skill_id
+--     --STRING_AGG(SD.skills,',') AS SKILLS
+--     FROM skills_dim SD
+-- INNER JOIN skills_job_dim SJD ON SJD.skill_id = SD.skill_id
+-- --GROUP BY SD.skill_id
+-- ORDER BY SD.skill_id ASC
+-- LIMIT 10)
 
-SELECT *
-    FROM SKILL_SETS
+
+
+SELECT 
+    --JPF.job_title_short AS JOB_NAME,
+    SD.skills AS SKILLS,
+    COUNT(SJD.job_id) AS DEMAND_COUNT
+    --JPF.job_location AS JOB_LOCATION,
+    --JPF.salary_year_avg AS SALARY
+    FROM job_postings_fact JPF 
+LEFT JOIN skills_job_dim SJD ON SJD.job_id = JPF.job_id
+LEFT JOIN skills_dim SD on SD.skill_id = SJD.skill_id
+WHERE JPF.job_title_short = 'Data Analyst'
+GROUP BY SKILLS
+ORDER BY DEMAND_COUNT DESC
+LIMIT 10
